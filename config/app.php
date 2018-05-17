43fca648354224049e12d37f00baa82142062d1c
Tonny H. Soerojo <tonny@soerojo.com>
2018-05-17 11:47:03 +0000

    clean code

--- config/app.php
+++ config/app.php
@@ -67,7 +67,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://legaltech.co.id/public/'),
 
     /*
     |--------------------------------------------------------------------------
