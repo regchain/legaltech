e908b8eabc6a55b5fe70e6f3cd20e8f73232b9e9
Tonny H Soerojo <tonny@soerojo.com>
2018-05-18 18:36:42 +0700

    backpack uninstalled

--- app/User.php
+++ app/User.php
@@ -4,21 +4,10 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Backpack\Base\app\Notifications\ResetPasswordNotification as ResetPasswordNotification;
 
 class User extends Authenticatable
 {
     use Notifiable;
-     /**
-   * Send the password reset notification.
-   *
-   * @param  string  $token
-   * @return void
-   */
-  public function sendPasswordResetNotification($token)
-  {
-      $this->notify(new ResetPasswordNotification($token));
-  }
 
     /**
      * The attributes that are mass assignable.
