8e47b13436547264dea20a71334533c1594c8c1b
Tonny H Soerojo <tonny@soerojo.com>
2018-05-18 18:43:03 +0700

    Jeroennoten AdminLTE

--- config/app.php
+++ config/app.php
@@ -151,6 +151,8 @@ return [
          * Package Service Providers...
          */
 
+         JeroenNoten\LaravelAdminLte\ServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
