2badaf3e65b3fde39ac2a1de514f8888b8ba0e88
Tonny H Soerojo <tonny@soerojo.com>
2018-05-18 11:45:32 +0700

    change app url

--- system/config/app.php
+++ system/config/app.php
@@ -67,7 +67,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://legaltech.co.id/public/'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     /*
     |--------------------------------------------------------------------------
