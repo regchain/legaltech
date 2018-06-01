627391a6fca0b0b47dfe397afb30b27de7c6dd8c
Tonny H. Soerojo <tonny@soerojo.com>
2018-06-01 13:24:33 +0700

    side menu intel updated

--- resources/views/themes/admin/menu/core/intel.blade.php
+++ resources/views/themes/admin/menu/core/intel.blade.php
@@ -1,16 +1,31 @@
 <li class="treeview">
+  <a href="#">
+    <i class="fa fa-fw fa-user-secret "></i>
+    <span>Intelijen</span>
+    <span class="pull-right-container">
+      <i class="fa fa-angle-left pull-right"></i>
+    </span>
+  </a>
+  <ul class="treeview-menu">
+    <li class="treeview">
       <a href="#">
-        <i class="fa fa-fw fa-user-secret "></i>
-        <span>Intelijen</span>
+        <i class="fa fa-fw fa-bar-chart "></i>
+        <span>TP4D</span>
         <span class="pull-right-container">
           <i class="fa fa-angle-left pull-right"></i>
         </span>
       </a>
       <ul class="treeview-menu">
+        <li class="">
+          <a href="#">
+            <i class="fa fa-fw fa-dashboard "></i>
+            <span>Dashboard</span>
+          </a>
+        </li>
         <li class="treeview">
           <a href="#">
-            <i class="fa fa-fw fa-bar-chart "></i>
-            <span>TP4D</span>
+            <i class="fa fa-fw fa-puzzle-piece "></i>
+            <span>PERENCANAAN</span>
             <span class="pull-right-container">
               <i class="fa fa-angle-left pull-right"></i>
             </span>
@@ -18,80 +33,28 @@
           <ul class="treeview-menu">
             <li class="">
               <a href="#">
-                <i class="fa fa-fw fa-dashboard "></i>
-                <span>Dashboard</span>
+                <i class="fa fa-fw fa-money "></i>
+                <span>e-Budgeting [SIMDA]</span>
               </a>
-            </li>
-            <li class="treeview">
-              <a href="#">
-                <i class="fa fa-fw fa-puzzle-piece "></i>
-                <span>Perencanaan</span>
-                <span class="pull-right-container">
-                  <i class="fa fa-angle-left pull-right"></i>
-                </span>
-              </a>
-              <ul class="treeview-menu">
-                <li class="">
-                  <a href="#">
-                    <i class="fa fa-fw fa-angle-right "></i>
-                    <span>eBudgeting [SIMDA]</span>
-                  </a>
-                </li>
-                <li class="">
-                  <a href="#">
-                    <i class="fa fa-fw fa-angle-right "></i>
-                    <span>eRUP [SIRUP]</span>
-                  </a>
-                </li>
-                <li class="">
-                  <a href="#">
-                    <i class="fa fa-fw fa-angle-right "></i>
-                    <span>Project Planning</span>
-                  </a>
-                </li>
-              </ul>
-            </li>
-            <li class="treeview">
-              <a href="#">
-                <i class="fa fa-fw fa-cubes "></i>
-                <span>Pengadaan</span>
-                <span class="pull-right-container">
-                  <i class="fa fa-angle-left pull-right"></i>
-                </span>
-              </a>
-              <ul class="treeview-menu">
-                <li class="">
-                  <a href="#">
-                    <i class="fa fa-fw fa-angle-right "></i>
-                    <span>eULP</span>
-                  </a>
-                </li>
-                <li class="">
-                  <a href="#">
-                    <i class="fa fa-fw fa-angle-right "></i>
-                    <span>eProc [LPSE]</span>
-                  </a>
-                </li>
-                <li class="">
-                  <a href="#">
-                    <i class="fa fa-fw fa-angle-right "></i>
-                    <span>eSwakelola</span>
-                  </a>
-                </li>
-              </ul>
             </li>
             <li class="">
               <a href="#">
-                <i class="fa fa-fw fa-file-text "></i>
-                <span>eKontrak</span>
+                <i class="fa fa-fw fa-history "></i>
+                <span>e-RUP [SIRUP]</span>
+              </a>
+            </li>
+            <li class="">
+              <a href="#">
+                <i class="fa fa-fw fa-calendar "></i>
+                <span>e-Project Planning</span>
               </a>
             </li>
           </ul>
         </li>
         <li class="treeview">
           <a href="#">
-            <i class="fa fa-fw fa-angle-right "></i>
-            <span>Apps 2</span>
+            <i class="fa fa-fw fa-balance-scale "></i>
+            <span>PENGADAAN</span>
             <span class="pull-right-container">
               <i class="fa fa-angle-left pull-right"></i>
             </span>
@@ -100,45 +63,111 @@
             <li class="">
               <a href="#">
                 <i class="fa fa-fw fa-angle-right "></i>
-                <span>Dashboard</span>
+                <span>e-ULP</span>
               </a>
             </li>
             <li class="">
               <a href="#">
                 <i class="fa fa-fw fa-angle-right "></i>
-                <span>Page 1</span>
+                <span>e-Proc [LPSE]</span>
               </a>
             </li>
-            <li class="treeview">
+            <li class="">
               <a href="#">
                 <i class="fa fa-fw fa-angle-right "></i>
-                <span>Progress 1</span>
-                <span class="pull-right-container">
-                  <i class="fa fa-angle-left pull-right"></i>
-                </span>
+                <span>e-Swakelola</span>
               </a>
-              <ul class="treeview-menu">
-                <li class="">
-                  <a href="#">
-                    <i class="fa fa-fw fa-angle-right "></i>
-                    <span>List</span>
-                  </a>
-                </li>
-                <li class="">
-                  <a href="#">
-                    <i class="fa fa-fw fa-angle-right "></i>
-                    <span>Edit</span>
-                  </a>
-                </li>
-                <li class="">
-                  <a href="#">
-                    <i class="fa fa-fw fa-angle-right "></i>
-                    <span>Process</span>
-                  </a>
-                </li>
-              </ul>
+            </li>
+            <li class="">
+              <a href="#">
+                <i class="fa fa-fw fa-angle-right "></i>
+                <span>e-Kontrak</span>
+              </a>
+            </li>
+          </ul>
+        </li>
+         <li class="treeview">
+          <a href="#">
+            <i class="fa fa-fw fa-eye "></i>
+            <span>MONEV</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li class="">
+              <a href="#">
+                <i class="fa fa-fw fa-tasks "></i>
+                <span>e-Progress</span>
+              </a>
+            </li>
+            <li class="">
+              <a href="#">
+                <i class="fa fa-fw fa-bar-chart "></i>
+                <span>e-Performance</span>
+              </a>
+            </li>
+            <li class="">
+              <a href="#">
+                <i class="fa fa-fw fa-bank "></i>
+                <span>e-Asset</span>
+              </a>
             </li>
           </ul>
         </li>
       </ul>
     </li>
+    <li class="treeview">
+      <a href="#">
+        <i class="fa fa-fw fa-angle-right "></i>
+        <span>Apps 2</span>
+        <span class="pull-right-container">
+          <i class="fa fa-angle-left pull-right"></i>
+        </span>
+      </a>
+      <ul class="treeview-menu">
+        <li class="">
+          <a href="#">
+            <i class="fa fa-fw fa-angle-right "></i>
+            <span>Dashboard</span>
+          </a>
+        </li>
+        <li class="">
+          <a href="#">
+            <i class="fa fa-fw fa-angle-right "></i>
+            <span>Page 1</span>
+          </a>
+        </li>
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-fw fa-angle-right "></i>
+            <span>Progress 1</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li class="">
+              <a href="#">
+                <i class="fa fa-fw fa-angle-right "></i>
+                <span>List</span>
+              </a>
+            </li>
+            <li class="">
+              <a href="#">
+                <i class="fa fa-fw fa-angle-right "></i>
+                <span>Edit</span>
+              </a>
+            </li>
+            <li class="">
+              <a href="#">
+                <i class="fa fa-fw fa-angle-right "></i>
+                <span>Process</span>
+              </a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </li>
+  </ul>
+</li>
