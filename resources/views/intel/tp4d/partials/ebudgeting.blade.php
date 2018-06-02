9d5fb8318bd585b2c1b20f344800ef187c2ff00f
Tonny H. Soerojo <tonny@soerojo.com>
2018-06-02 15:39:35 +0700

    ebudgeting

--- resources/views/intel/tp4d/partials/ebudgeting.blade.php
+++ resources/views/intel/tp4d/partials/ebudgeting.blade.php
@@ -4,7 +4,7 @@
             <span class="pull-right"><a href="ebudgeting"><i class="fa fa-forward"></i> Lihat detil</a></span> <h3 class="box-title">e-Budgeting</h3>
           </div>
           <!-- /.box-header -->
-          {{-- <div class="box-body">
+          <div class="box-body">
             <div class="col-md-3 alert alert-danger alert-dismissible">
               <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
               <h4><i class="icon fa fa-ban"></i> Alert!</h4>
@@ -25,11 +25,11 @@
               <h4><i class="icon fa fa-check"></i> Alert!</h4>
               Success alert preview. This alert is dismissable.
             </div>
-          </div> --}}
+          </div>
           <!-- /.box-body -->
           <div class="box-body">
             <div class="col-md-12">
-              <div class="even"><section class="mdc-card__primary"><h1 class="mdc-card__title mdc-card__title--large f300">Belanja Modal Peralatan dan Mesin Pengadaan Mebelair</h1><h2 class="mdc-card__subtitle">Kota Bandung - DINAS KESEHATAN</h2><h3 class="mdc-card__subtitle dark-gray">SirupID: #347</h3></section><section class="mdc-card__supporting-text "><div class="procurement-card-container flex"><div class="procurement-card-details padding-small text-center"><img class="icon-large" src="img/icon-money.png"><div class="mdc-typography--subheading1"> Pagu </div><div> <span class="mdc-typography--title f300"> 0.00092 </span> M</div></div><div class="procurement-card-details padding-small text-center"><img class="icon-large" src="img/icon-gov.png"> <div class="mdc-typography--subheading1"> Budget </div> <div class="mdc-typography--title f300"> APBD </div></div><div class="procurement-card-details padding-small text-center"> <img class="icon-large" src="img/icon-tender-start.png"> <div class="mdc-typography--subheading1"> Tender start </div><div class="mdc-typography--title f300"> Feb 1, 2017 </div></div><div class="procurement-card-details padding-small text-center"><img class="icon-large" src="img/icon-tender-end.png"> <div class="mdc-typography--subheading1"> Tender end</div><div class="mdc-typography--title f300"> Feb 1, 2017 </div> </div> <div class="procurement-card-details padding-small text-center "><img class="icon-large" src="img/icon-contract-start.png"><div class="mdc-typography--subheading1">Contract start</div><div class="mdc-typography--title f300">Feb 1, 2017</div></div> <div class="procurement-card-details padding-small text-center"><img class="icon-large" src="img/icon-contract-end.png"><div class="mdc-typography--subheading1">Contract end</div> <div class="mdc-typography--title f300">Feb 1, 2017</div></div> </div> <div><p>This contract is for <i> <u> services</u></i> in the program <u><i>Program Peningkatan Kualitas Hidup dan Perlindungan Perempuan</i></u>. The procurement method is <u><i> Sayembara Kontes </i></u>. The award criteria is <i> <u>bestProposal</u></i>.</p> </div></section><section class="mdc-card__actions"> <button class="mdc-button mdc-button--compact mdc-card__action"> Apply in Sirup </button><button class="mdc-button mdc-button--compact mdc-card__action"> Download </button> <button class="mdc-button mdc-button--compact mdc-card__action">Email</button></section></div>
+              
             </div>
           </div>
         </div>
